4267f33d32ee206def04561941b9ecd34b3c15fa
abdi42 <abdullahimahamed42@gmail.com>
2016-12-21 22:43:09 -0600

    init commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+var dockerhttp = require("./lib/dockerhttp.js");
+var dockerApi = dockerhttp('127.0.0.1','4243');
+
+console.log(dockerApi);
---
+++ src/lib/dockerhttp.js
@@ -0,0 +1,49 @@
+var request = require("request");
+
+var DockerApi = function(apiUrl,port){
+  var url = apiUrl + ":" + port;
+
+  return {
+    get:function(endpoint,callback){
+        request({
+            url:url + endpoint,
+            method:"GET",
+        },function(err,response,body){
+            if(err)
+                return callback(response.statusCode + " " + body);
+
+            return callback(null,body);
+        })
+    },
+    post:function(endpoint,_body,callback){
+        request({
+            url:url + endpoint,
+            method:"POST",
+            body:_body,
+            json:true
+        },function(err,response,body){
+            if(err)
+                return callback(response.statusCode + " " + body);
+
+            return callback(null,body);
+        })
+    },
+    delete:function(endpoint,_body,callback){
+        request({
+            url:url + endpoint,
+            method:"DELETE",
+            body:_body,
+            json:true
+        },function(err,response,body){
+            if(err)
+                return callback(response.statusCode + " " + body);
+
+            return callback(null,body);
+        })
+    }
+  }
+}
+
+
+
+module.exports = DockerApi;
